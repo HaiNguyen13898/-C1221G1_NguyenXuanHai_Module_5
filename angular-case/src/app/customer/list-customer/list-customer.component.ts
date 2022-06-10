95aaf177412afbd758e883a598f50beab42f8b7f
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-06-10 19:51:26 +0700

    nop bai

--- angular-case/src/app/customer/list-customer/list-customer.component.ts
+++ angular-case/src/app/customer/list-customer/list-customer.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {Customer} from "../../model/customer";
 
 @Component({
   selector: 'app-list-customer',
   templateUrl: './list-customer.component.html',
   styleUrls: ['./list-customer.component.css']
+
 })
 export class ListCustomerComponent implements OnInit {
+  customers: Customer[] = [];
+  id: number;
 
-  constructor() { }
+  constructor() {
+    this.customers.push({
+      idCustomer: 1,
+      nameCustomer: 'Nguyễn Xuân Tùng',
+      dateBirth: '2022-05-12',
+      gender: 'Nam',
+      idCard: '131234',
+      phone: '092193123',
+      email: 'tung@gmail.com',
+      address: '94 Lê Qunag Đạo',
+      cusType: 'Platinium'
+    })
+    this.customers.push({
+      idCustomer: 2,
+      nameCustomer: 'Phạm Xuân Diệu',
+      dateBirth: '1992-08-08',
+      gender: 'Nam',
+      idCard: '865342123',
+      phone: '0954333333',
+      email: 'xuandieu92@gmail.com',
+      address: 'K77/22 Thái Phiên, Quảng Trị',
+      cusType: 'Gold'
+    })
+    this.customers.push({
+      idCustomer: 3,
+      nameCustomer: 'Trương Đình Nghệ',
+      dateBirth: '1990-02-27',
+      gender: 'Nam',
+      idCard: '488645199',
+      phone: '0373213122',
+      email: 'nghenhan2702@gmail.com',
+      address: 'K323/12 Ông Ích Khiêm, Vinh',
+      cusType: 'Diamond'
+    })
+    this.customers.push({
+      idCustomer: 4,
+      nameCustomer: 'Dương Văn Quan',
+      dateBirth: '1981-07-08',
+      gender: 'Nam',
+      idCard: '543432111',
+      phone: '0490039241',
+      email: 'duongquan@gmail.com',
+      address: 'K453/12 Lê Lợi, Đà Nẵng',
+      cusType: 'Diamond'
+    })
+  }
 
   ngOnInit(): void {
   }
 
+
+  deleteCustomer() {
+
+  }
 }
---
+++ angular-case/src/app/model/customer.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+idCustomer: number;
+nameCustomer: string;
+dateBirth: string;
+gender: string;
+idCard: string;
+phone: string;
+email: string;
+address: string;
+cusType:string;
+}
