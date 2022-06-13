b54289168b12024a198ce33ae7d0cec25a7b92fc
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2022-06-13 20:23:46 +0700

    nop bai

--- ss6-Angular-Form/bai_tap/angular-register/src/app/register/register.component.ts
+++ ss6-Angular-Form/bai_tap/angular-register/src/app/register/register.component.ts
@@ -10,7 +10,7 @@ export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
-    this.registerForm = new FormGroup({
+    this.registerForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.pattern('^\\w+([.-]?\\w+)*@[a-z]+\\.(\\w+){2,}(\\.\\w{2,3})?')]),
       password: new FormControl('', [Validators.minLength(6), Validators.required]),
       confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
@@ -18,7 +18,7 @@ export class RegisterComponent implements OnInit {
       age: new FormControl('', [Validators.min(18), Validators.required, Validators.max(100)]),
       gender: new FormControl(),
       phone: new FormControl('', Validators.pattern('/^\\+84\\d{9,10}$/')),
-    }, {validator: this.MustMatch('password', 'confirmPassword')); // Đang lỗi, không thể gán 2 tham số vào được
+    }, {validator: this.MustMatch('password', 'confirmPassword')});
   }
 
   get f() {
@@ -33,11 +33,11 @@ export class RegisterComponent implements OnInit {
         return;
       }
       if (control.value !== matchingControl.value) {
-        matchingControl.setErrors({ mustMatch: true });
+        matchingControl.setErrors({mustMatch: true});
       } else {
         matchingControl.setErrors(null);
       }
-    }
+    };
   }
 
   ngOnInit(): void {
